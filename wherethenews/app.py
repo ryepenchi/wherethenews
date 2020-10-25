daa01ec298b82529f38cb03a42dc872db736fbb4
ryepenchi <ryepenchi@gmail.com>
2020-10-25 20:34:04 +0100

    cards

--- wherethenews/app.py
+++ wherethenews/app.py
@@ -13,52 +13,8 @@ db = SQLAlchemy(app)
 
 from dbconfig import Article, Place
 
-#BASECOORDS = [-13.9626, 33.7741]
-BASECOORDS = [48.210033,16.363449]
-
-# class Point(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     latitude_off = db.Column(db.Float)
-#     longitude_off = db.Column(db.Float)
-#     district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
-#     district = db.relationship("District")
-
-#     def __init__(self, id, district, lat, lng):
-#         self.id = id
-#         self.district = district
-#         self.latitude_off = lat
-#         self.longitude_off = lng
-
-#     def __repr__(self):
-#         return "<Point %d: Lat %s Lng %s>" % (self.id, self.latitude_off, self.longitude_off)
-
-#     @property
-#     def latitude(self):
-#         return self.latitude_off + self.district.latitude
-
-#     @property
-#     def longitude(self):
-#         return self.longitude_off + self.district.longitude
-
-
-# class District(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80))
-#     latitude = db.Column(db.Float)
-#     longitude = db.Column(db.Float)
-
-#     def __init__(self, id, name, lat, lng):
-#         self.id = id
-#         self.name = name
-#         self.latitude = lat
-#         self.longitude = lng
-
-
 @app.route('/')
 def index():
-    # districts = District.query.all()
-    # return render_template('index.html', districts=districts)
-    # places = Place.query.all()
     return render_template('index.html')
 
 @app.route('/points')
@@ -70,46 +26,23 @@ def points():
     to_date = dp.parse(to_date)
     print(from_date, to_date)
     # points = Place.query.select_from(Place,Article).filter(from_date < Article.pub_date, Article.pub_date < to_date).all()
+    # Until Query works just get everything
     points = Place.query.all()
+    articles = Article.query.all()
     print(points)
-    coords = []
+    p = []
     for point in points:
-        pointname = "<b>" + point.word + "</b><br>"
-        links = "".join([f"<a href='{a.link}' target='_blank'>{a.title[:30]}...</a><br>" for a in point.articles])
-        markertext = pointname + links
-        coords.append([point.lat, point.lon, markertext, point.word, links])
-    return jsonify({"data": coords})
-
-# @app.route('/district/<int:district_id>')
-# def district(district_id):
-#     points = Place.query.all()
-#     coords = []
-#     for point in points:
-#         pointname = "<b>" + point.word + "</b><br>"
-#         links = "".join([f"<a href='{a.link}' target='_blank'>{a.title[:30]}...</a><br>" for a in point.articles])
-#         markertext = pointname + links
-#         coords.append([point.lat, point.lon, markertext])
-#     return jsonify({"data": coords})
-
-
-# def make_random_data(db):
-#     NDISTRICTS = 5
-#     NPOINTS = 10
-#     for did in range(NDISTRICTS):
-#         district = District(did, "District %d" % did, BASECOORDS[0], BASECOORDS[1])
-#         db.session.add(district)
-#         for pid in range(NPOINTS):
-#             lat = random.random() - 0.5
-#             lng = random.random() - 0.5
-#             row = Point(pid + NPOINTS * did, district, lat, lng)
-#             db.session.add(row)
-#     db.session.commit()
-
+        links = [a.link for a in point.articles]
+        p.append({"word":point.word, "links":links, "lat":point.lat, "lon":point.lon})
+    a = []
+    for article in articles:
+        words = [p.word for p in article.places]
+        a.append({"title":article.title, "words":words, "link":article.link})
+    return jsonify({"points": p, "articles": a})
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'mkdb':
             db.create_all()
-            # make_random_data(db)
     else:
         app.run(host="0.0.0.0", debug=True)
--- wherethenews/static/js/main.js
+++ wherethenews/static/js/main.js
@@ -1,5 +1,5 @@
 
-var map = L.map('llmap').fitWorld();
+var map = L.map('llmap').setView([48.517587, 8.648699], 5);
 
 // var TILE_URL = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWhvcm5zaXJ1cCIsImEiOiJjazNqNTBxeHgwM2trM2RydnozbDdwMXMwIn0.0xe5TIh6XSo1pKrjsAUgEA";
 var TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,24 +14,6 @@ L.tileLayer(TILE_URL, {
 	// zoomOffset: -1
 }).addTo(map);
 
-function onLocationFound(e) {
-	var radius = e.accuracy / 2;
-
-	L.marker(e.latlng).addTo(map)
-		.bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-	L.circle(e.latlng, radius).addTo(map);
-}
-
-function onLocationError(e) {
-	alert(e.message);
-}
-
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
-
-map.locate({setView: true, maxZoom: 16});
-
 function renderData() {
 	var request = new XMLHttpRequest();
 	var from_date = new Date();
@@ -43,29 +25,51 @@ function renderData() {
 	request.onload = function() {
 		if (this.status >= 200 && this.status < 400) {
 			// Success
+			// Create MapMarkers
 			var data = JSON.parse(this.response);
-			var markers = data.data.map(function(arr) {
-				return L.marker([arr[0], arr[1]]).bindPopup(arr[2]);
+			var markers = data.points.map(function(arr) {
+				const t = document.createElement("b");
+				t.innerHTML = arr.word + "<br>";
+				const links = arr.links.map(function (l) {
+					//<a href='{a.link}' target='_blank' class='truncate'>{a.title}...</a><br>
+					const a = document.createElement("a");
+					a.href = l;
+					a.className = "truncate";
+					a.innerText = l;
+					a.target = "_blank";
+					const b = document.createElement("br");
+					a.append(b);
+					t.append(a);
+				});
+				return L.marker([arr.lat, arr.lon]).bindPopup(t);
 			});
 			map.removeLayer(layer);
 			layer = L.layerGroup(markers);
 			map.addLayer(layer);
 			// Create Cards
-			data.data.map(function (arr) {
-				var card = document.createElement("div")
+			data.articles.map(function (arr) {
+				const span = document.createElement("span");
+				span.className = "card-title";
+				span.innerText = arr.title;
+				const para = document.createElement("p");
+				para.className = "truncate";
+				para.innerHTML = arr.words;
+				const d = document.createElement("div");
+				d.className = "card-content white-text";
+				d.appendChild(span);
+				d.appendChild(para);
+				// const diva = document.createElement("div");
+				// diva.className = "card-action";
+				// diva.innerHTML = arr.link;
+				const card = document.createElement("div")
 				card.className = "card blue-grey lighten-1 collection-item"
-				card.innerHTML = `
-				<div class="card-content white-text">
-				  <span id="card-title" class="card-title">Placename</span>
-				  <p id="card-p" class="truncate">Titel des Artikels, könnte auch mal länger sein, aber was solls</p>
-				</div>
-				<div id="card-action" class="card-action">
-				  <a href="#">Link zum Artikel</a>
-				</div>
-				`
-				// card.getElementById("card-title").innerHTML = arr[3];
-				// card.getElementById("card-action").innerHTML = arr[4];
-				document.getElementById("card-collection").appendChild(card);
+				card.appendChild(d);
+				// card.appendChild(diva);
+				const cardlink = document.createElement("a");
+				cardlink.href = arr.link;
+				cardlink.target = "_blank";
+				cardlink.appendChild(card);
+				document.getElementById("card-collection").appendChild(cardlink);
 			});
 		} else {
 			//Reached target Server, but it returned an error
