4f706502b6e281bf2f6aa7c48529cddac0c9c699
ryepenchi <ryepenchi@gmail.com>
2020-10-28 21:41:51 +0100

    today is 0-2359

--- wherethenews/static/js/main.js
+++ wherethenews/static/js/main.js
@@ -98,8 +98,9 @@ window.onload = function () {
 	renderData();
 };
 
-var from_date = new Date();
-var to_date = new Date();
+var today = new Date();
+var from_date = new Date(today.getFullYear(), today.getMonth(), today.getDate(),0,0);
+var to_date = new Date(today.getFullYear(), today.getMonth(), today.getDate(),23,59);
 
 // Date Buttons
 document.getElementById("m1d").onclick = function () {
